b52c5b422beebef4c0298081bc53e5952dff77eb
Darryl Eychner <darryl.eychner@blackbaud.com>
2019-07-02 14:18:04 -0400

    Update infinity.spec.js
    Fixes failures for the infinity utilities tests.

--- Blackbaud.AppFx.MajorGiving.Mobile.Sky/test/utils/infinity.spec.js
+++ Blackbaud.AppFx.MajorGiving.Mobile.Sky/test/utils/infinity.spec.js
@@ -6,32 +6,30 @@
 
     describe('utils.infinity', function () {
 
-        var infinityUtilities,
-            windowLocation;
+        var infinityUtilities;
 
         beforeEach(function () {
 
-            windowLocation = {
-                href: "http://MockHost/MockPath/sky/frog/?databaseName=BBInfinityMock",
-                replace: angular.noop
-            };
-
             module('sky.moment');
-
-            module('infinity.util');
 
             module('frog.util');
 
+            module('infinity.util');
 
             module(function ($provide, mockableUtilitiesProvider) {
 
                 var mockUtil = mockableUtilitiesProvider.$get();
                 mockUtil.getWindowLocation = function () {
-                    return windowLocation;
+                    return {
+                        href: "http://MockHost/MockPath/sky/frog/?databaseName=BBInfinityMock",
+                        replace: angular.noop
+                    };
                 };
                 $provide.value("mockableUtilities", mockUtil);
 
             });
+
+            module('frog.api');
 
         });
 
