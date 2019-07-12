46eb4e6062592e129a12680c6804974ce49f947c
Darryl Eychner <darryl.eychner@blackbaud.com>
2019-07-12 16:52:34 -0400

    Update crm.js
    Update API file to include some missing references.

--- Blackbaud.AppFx.MajorGiving.Mobile.Sky/src/api/crm.js
+++ Blackbaud.AppFx.MajorGiving.Mobile.Sky/src/api/crm.js
@@ -7,7 +7,7 @@
 
     var noop = angular.noop;
 
-    angular.module("frog.api", ["frog.util", "bbui", "sky.moment", "frog.resources"])
+    angular.module("frog.api", ["frog.util", "infinity.util", "bbui", "sky.moment", "frog.resources"])
         .factory("apiPortfolio", ["bbuiShellService", "frogResources", "prospectUtilities", "infinityCache", "bbui", "$q", "infinityUtilities",
             function (bbuiShellService, frogResources, prospectUtilities, infinityCache, bbui, $q, infinityUtilities) {
 
@@ -2618,54 +2618,6 @@
 
             }])
 
-        .factory("api", ["infinityUtilities", "infinityAuth", "apiPortfolio", "apiProspectView", "apiContactReportOptions", "apiContactReport", "infinityProduct", 'customizable',
-            function (infinityUtilities, infinityAuth, apiPortfolio, apiProspectView, apiContactReportOptions, apiContactReport, infinityProduct, customizable) {
-
-                function initialize() {
-                    infinityUtilities.initialize(customizable.getRootFolder(), customizable.isCustomApp());
-                }
-
-                return {
-                    getDatabaseName: infinityUtilities.getDatabaseName,
-                    initialize: initialize,
-                    authenticateAsync: infinityAuth.authenticateAsync,
-                    logoutAsync: infinityAuth.logoutAsync,
-                    getPortfolioAsync: apiPortfolio.getPortfolioAsync,
-                    getPortfolioSettingsAsync: apiPortfolio.getPortfolioSettingsAsync,
-                    getConstituentSearchListInformationAsync: apiPortfolio.getConstituentSearchListInformationAsync,
-                    getProspectInfoAsync: apiProspectView.getProspectInfoAsync,
-                    getProspectSummaryAsync: apiProspectView.getProspectSummaryAsync,
-                    getRecentStepsAsync: apiProspectView.getRecentStepsAsync,
-                    getConstituentSearchResultsAsync: apiPortfolio.getConstituentSearchResultsAsync,
-                    getCountriesListAsync: apiPortfolio.getCountriesListAsync,
-                    getStatesListAsync: apiPortfolio.getStatesListAsync,
-                    getDefaultCountryAsync: apiPortfolio.getDefaultCountryAsync,
-                    getCountryFormatsAsync: apiPortfolio.getCountryFormatsAsync,
-                    getRecentGiftsAndCreditsAsync: apiProspectView.getRecentGiftsAndCreditsAsync,
-                    getAddressesListAsync: apiProspectView.getAddressesListAsync,
-                    getAdditionalRevenueDetailsAsync: apiProspectView.getAdditionalRevenueDetailsAsync,
-                    getContactMethodsAsync: apiContactReportOptions.getContactMethodsAsync,
-                    getPlanStagesAsync: apiContactReportOptions.getPlanStagesAsync,
-                    getCategoriesAsync: apiContactReportOptions.getCategoriesAsync,
-                    getSubcategoriesAsync: apiContactReportOptions.getSubcategoriesAsync,
-                    getLocationsAsync: apiContactReportOptions.getLocationsAsync,
-                    getPotentialParticipantsAsync: apiContactReportOptions.getPotentialParticipantsAsync,
-                    getPotentialSolicitorsAsync: apiContactReportOptions.getPotentialSolicitorsAsync,
-                    getSitesAsync: apiContactReportOptions.getSitesAsync,
-                    getPlansAsync: apiContactReportOptions.getPlansAsync,
-                    getStatusCodesAsync: apiContactReportOptions.getStatusCodesAsync,
-                    getCompletedStatusCode: apiContactReportOptions.getCompletedStatusCode,
-                    loadStepAsync: apiContactReport.loadStepAsync,
-                    addStepAsync: apiContactReport.addStepAsync,
-                    editStepAsync: apiContactReport.editStepAsync,
-                    getContactReportPreloadAsync: apiContactReport.getContactReportPreloadAsync,
-                    productIsInstalledAsync: infinityProduct.productIsInstalledAsync,
-                    getAuthInterceptors: infinityAuth.getAuthInterceptors
-                };
-
-            }
-        ])
-
         .factory('customizableRoot', [function () {
             return {
 
@@ -2714,5 +2666,53 @@
 
                 // Add other custom components here.
             };
-        }]);
+        }])
+
+        .factory("api", ["infinityUtilities", "infinityAuth", "apiPortfolio", "apiProspectView", "apiContactReportOptions", "apiContactReport", "infinityProduct", 'customizable',
+            function (infinityUtilities, infinityAuth, apiPortfolio, apiProspectView, apiContactReportOptions, apiContactReport, infinityProduct, customizable) {
+
+                function initialize() {
+                    infinityUtilities.initialize(customizable.getRootFolder(), customizable.isCustomApp());
+                }
+
+                return {
+                    getDatabaseName: infinityUtilities.getDatabaseName,
+                    initialize: initialize,
+                    authenticateAsync: infinityAuth.authenticateAsync,
+                    logoutAsync: infinityAuth.logoutAsync,
+                    getPortfolioAsync: apiPortfolio.getPortfolioAsync,
+                    getPortfolioSettingsAsync: apiPortfolio.getPortfolioSettingsAsync,
+                    getConstituentSearchListInformationAsync: apiPortfolio.getConstituentSearchListInformationAsync,
+                    getProspectInfoAsync: apiProspectView.getProspectInfoAsync,
+                    getProspectSummaryAsync: apiProspectView.getProspectSummaryAsync,
+                    getRecentStepsAsync: apiProspectView.getRecentStepsAsync,
+                    getConstituentSearchResultsAsync: apiPortfolio.getConstituentSearchResultsAsync,
+                    getCountriesListAsync: apiPortfolio.getCountriesListAsync,
+                    getStatesListAsync: apiPortfolio.getStatesListAsync,
+                    getDefaultCountryAsync: apiPortfolio.getDefaultCountryAsync,
+                    getCountryFormatsAsync: apiPortfolio.getCountryFormatsAsync,
+                    getRecentGiftsAndCreditsAsync: apiProspectView.getRecentGiftsAndCreditsAsync,
+                    getAddressesListAsync: apiProspectView.getAddressesListAsync,
+                    getAdditionalRevenueDetailsAsync: apiProspectView.getAdditionalRevenueDetailsAsync,
+                    getContactMethodsAsync: apiContactReportOptions.getContactMethodsAsync,
+                    getPlanStagesAsync: apiContactReportOptions.getPlanStagesAsync,
+                    getCategoriesAsync: apiContactReportOptions.getCategoriesAsync,
+                    getSubcategoriesAsync: apiContactReportOptions.getSubcategoriesAsync,
+                    getLocationsAsync: apiContactReportOptions.getLocationsAsync,
+                    getPotentialParticipantsAsync: apiContactReportOptions.getPotentialParticipantsAsync,
+                    getPotentialSolicitorsAsync: apiContactReportOptions.getPotentialSolicitorsAsync,
+                    getSitesAsync: apiContactReportOptions.getSitesAsync,
+                    getPlansAsync: apiContactReportOptions.getPlansAsync,
+                    getStatusCodesAsync: apiContactReportOptions.getStatusCodesAsync,
+                    getCompletedStatusCode: apiContactReportOptions.getCompletedStatusCode,
+                    loadStepAsync: apiContactReport.loadStepAsync,
+                    addStepAsync: apiContactReport.addStepAsync,
+                    editStepAsync: apiContactReport.editStepAsync,
+                    getContactReportPreloadAsync: apiContactReport.getContactReportPreloadAsync,
+                    productIsInstalledAsync: infinityProduct.productIsInstalledAsync,
+                    getAuthInterceptors: infinityAuth.getAuthInterceptors
+                };
+
+            }
+        ]);
 }());
