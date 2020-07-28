b14608eef393756707a81e54c4f12285fde00518
Aaron Eberhart <Aaron.Eberhart@blackbaud.me>
2020-07-28 15:42:38 -0400

    Eliminating the isCrmInstallation flag.  It was superfluous to try and determine the full vroot path later on, we can just provide the 1 path as a variable and allow the client to update the path.  As an added bonus, this allows the client to plug in whatever path they want rather than being restricted to programfiles blackbaud.

--- Blackbaud.AppFx.MajorGiving.Mobile.Sky/gruntfile.js
+++ Blackbaud.AppFx.MajorGiving.Mobile.Sky/gruntfile.js
@@ -12,8 +12,10 @@ module.exports = function (grunt) {
         isDesktopBuild = grunt.option("IsDesktopBuild"),
         initialForceState = grunt.option("force"),
         testResultsFolder,
-        isCrmInstallation = true,
-        vroot = '';
+        vroot = '../../../../Blackbaud.AppFx.Server/Deploy/';
+    /*This is an example vroot for a client installation of frogger, replace VROOTFOLDERNAME with your value
+     * vroot = 'C:/Program Files/Blackbaud/VROOTFOLDERNAME/bbappfx/vroot/';
+     */
 
     console.log("Running Blackbaud.AppFx.MajorGiving.Mobile.Sky/gruntfile.js");
     console.log("IsDesktopBuild: " + isDesktopBuild);
@@ -42,13 +44,7 @@ module.exports = function (grunt) {
     }
 
     function getDestinationFolder(isCustomApp, rootFolder) {
-        var result = '';
-
-        if (isCrmInstallation) {
-            result += 'C:/Program Files/Blackbaud/' + vroot + 'bbappfx/vroot/';
-        } else {
-            result += '../../../../Blackbaud.AppFx.Server/Deploy/';
-        }
+        var result = vroot;
 
         if (isCustomApp) {
             result += 'browser/htmlforms/custom/';
